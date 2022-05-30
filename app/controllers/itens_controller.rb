1f00e96569735c43c9f0ea7e5932f7a3453fc2a1
gessica da silva <gessicahugendobler@gmail.com>
2022-05-30 15:06:32 -0300

    item model created

---
+++ app/controllers/itens_controller.rb
@@ -0,0 +1,28 @@
+class ItensController < ApplicationController
+  def index
+    @itens = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to @item
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
+end
---
+++ app/models/rent.rb
@@ -0,0 +1,4 @@
+class Rent < ApplicationRecord
+  belongs_to :user
+  belongs_to :item
+end
